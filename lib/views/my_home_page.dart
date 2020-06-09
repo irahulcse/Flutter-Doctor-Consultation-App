2606919dba658d262f6e3dda6cc3f49cc9501131
Rahul Chandra <1rahulchandra1@gmail.com>
2020-06-09 11:17:52 +0530

    Automatic Commit 2020-06-09-11:17:52

--- lib/views/my_home_page.dart
+++ lib/views/my_home_page.dart
@@ -1,11 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_doctor_consulant_app/constants.dart';
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //appBar:  ,
-      body: Container(),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(70.0),
+        child: Padding(
+          padding: EdgeInsets.only(top: 25),
+          child: ListTile(
+            leading: Container(
+              width: 40.0,
+              height: 40.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(
+                  12,
+                ),
+                image: DecorationImage(
+                  image: AssetImage(
+                    "assetName",
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            title: Text(
+              "Melody",
+              style: kTitleStyle,
+            ),
+            subtitle: Text(
+              "Find your Doctor's nearby",
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
